e7253c3864a5f7c03fae15635e8db77dce483500
Yu-Cheng Chuang <ducksteven@gmail.com>
2012-04-15 00:14:07 +0800

    implement getShortenedUrl() to send request to background.js and receive the result

--- popup.js
+++ popup.js
@@ -2,6 +2,30 @@ var GitIOPopup = new (function() {
   var text_field = document.getElementById("shortened-url");
   var message_container = document.getElementById("message");
 
+  this.getShortenedUrl = function() {
+
+    // XXX: tabs.getSelected is a private API.
+    // dirty, but works.
+    chrome.tabs.getSelected(null, function(tab) {
+
+      var request_data = {
+        "command": "generate",
+        "params": {
+          "url": tab.url
+        }
+      };
+
+      chrome.extension.sendRequest(request_data, function(data) {
+        if (data.status === "OK") {
+          setTextField(data.shortened_url);
+          setMessage("Generated.");
+        } else {
+          setMessage(data.error.message);
+        }
+      });
+    });
+  };
+
   var setTextField = function(text) {
     text_field.value = text;
   };
