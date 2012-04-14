39b0dd8daea9f15df80886ee1dce5eb0f8f951bd
Yu-Cheng Chuang <ducksteven@gmail.com>
2012-04-15 00:46:08 +0800

    Implement copy to clipboard functionality

--- popup.js
+++ popup.js
@@ -18,12 +18,27 @@ var GitIOPopup = new (function() {
       chrome.extension.sendRequest(request_data, function(data) {
         if (data.status === "OK") {
           setTextField(data.shortened_url);
-          setMessage("Generated.");
+          setMessage("Generated. Click to copy.");
         } else {
           setMessage(data.error.message);
         }
       });
     });
+  };
+
+  this.copyResultToClipboard = function() {
+    if (getTextField() === "") {
+      setMessage("Not yet generated...");
+    } else {
+      text_field.select();
+      document.execCommand('Copy');
+
+      setMessage("Copied to clipboard.");
+    }
+  };
+
+  var getTextField = function() {
+    return text_field.value;
   };
 
   var setTextField = function(text) {
@@ -37,4 +52,9 @@ var GitIOPopup = new (function() {
 
 document.addEventListener("DOMContentLoaded", function () {
   GitIOPopup.getShortenedUrl();
+});
+
+// copy the result to clipboard when click on #result or #shortened-url
+document.getElementById("shortened-url").addEventListener("click", function() {
+  GitIOPopup.copyResultToClipboard();
 });
