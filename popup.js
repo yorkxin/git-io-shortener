f59ba235e08f7574796fb804f81d34d8891e725b
Yu-Cheng Chuang <ducksteven@gmail.com>
2012-04-15 00:12:45 +0800

    add outlets for the shortened url textfield, and message section

--- popup.js
+++ popup.js
@@ -1,2 +1,12 @@
 var GitIOPopup = new (function() {
+  var text_field = document.getElementById("shortened-url");
+  var message_container = document.getElementById("message");
+
+  var setTextField = function(text) {
+    text_field.value = text;
+  };
+
+  var setMessage = function(message) {
+    message_container.innerText = message;
+  };
 })();
